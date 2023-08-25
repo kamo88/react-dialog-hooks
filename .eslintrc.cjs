d0fc9305726a647101ce0debf8cbb431a5bd7915
kamo88 <62532951+kamo88@users.noreply.github.com>
2023-08-25 23:03:44 +0900

    Add lint setting (#2)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,9 +2,11 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    'airbnb',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -14,5 +16,21 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'import/no-unresolved': ['error', { ignore: ['\\.svg$'] }],
   },
-}
+  overrides: [
+    {
+      files: ['vite.config.ts'],
+      rules: {
+        'import/no-extraneous-dependencies': 'off',
+      },
+    },
+  ],
+};
