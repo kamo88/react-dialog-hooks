9cd9349aa8a41553bf7396fd811309be291a481d
kamo88 <62532951+kamo88@users.noreply.github.com>
2023-08-26 16:38:27 +0900

    Change vite build setting for es & umd

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export {
+  useDialog,
+  useDialogPromise,
+  DialogResponse,
+  Dialog,
+} from '@/components/Dialog';
+export type { Props } from '@/components/Dialog';
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,43 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: '@kamo88-dialog', // グローバル変数として公開するライブラリの変数名
+      formats: ['es', 'umd'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'react-dom',
+        },
+      },
+    },
+  },
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    dts({
+      pathsToAliases: true,
+      include: [
+        path.resolve(__dirname, 'src/components/Dialog/hooks'),
+        path.resolve(__dirname, 'src/components/Dialog/Dialog.tsx'),
+        path.resolve(__dirname, 'src/components/Dialog/index.ts'),
+      ],
+      outDir: path.resolve(__dirname, 'dist/types'),
+    }),
+  ],
 });
