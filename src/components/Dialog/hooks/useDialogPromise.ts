fd151aad541fc0944c0aad5967909e89198e1f58
kamo88 <62532951+kamo88@users.noreply.github.com>
2023-08-26 15:17:28 +0900

    Feature/create dialog component (#9)
    * Add Dialog
    Component
    hooks
    
    * feat: Add Dialog storybook
    useDialog
    
    * feat: Add element in body for portal
    
    * Change customs docs
    
    * Add DialogPromise`s storybook
    
    * feat: Remove initical stories

---
+++ src/components/Dialog/hooks/useDialogPromise.ts
@@ -0,0 +1,67 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+export const DialogResponse = {
+  main: 'main',
+  sub: 'sub',
+  abort: 'abort',
+} as const;
+
+export type DialogResponseState =
+  (typeof DialogResponse)[keyof typeof DialogResponse];
+
+export const useDialogPromise = () => {
+  const ref = useRef<HTMLDialogElement>(null);
+
+  const promiseState = useRef(
+    new Promise<DialogResponseState>((resolve) => {
+      resolve('abort');
+    }),
+  );
+
+  const resolveState = useRef<(value: DialogResponseState) => void>(
+    () => () => {
+      // noop
+    },
+  );
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const showDialog = useCallback(() => {
+    resolveState.current('abort');
+    setIsOpen(true);
+    ref.current?.showModal();
+    promiseState.current = new Promise<DialogResponseState>((resolve) => {
+      resolveState.current = resolve;
+    });
+    return promiseState.current;
+  }, []);
+
+  const closeDialogMain = useCallback(() => {
+    setIsOpen(false);
+    ref.current?.close();
+    resolveState.current('main');
+  }, []);
+
+  const closeDialogSub = useCallback(() => {
+    setIsOpen(false);
+    ref.current?.close();
+    resolveState.current('sub');
+  }, []);
+
+  const closeDialogAbort = useCallback(() => {
+    setIsOpen(false);
+    ref.current?.close();
+    resolveState.current('abort');
+  }, []);
+
+  useEffect(() => () => closeDialogAbort(), [closeDialogAbort]);
+
+  return {
+    ref,
+    isOpen,
+    showDialog,
+    closeDialogMain,
+    closeDialogSub,
+    closeDialogAbort,
+  };
+};
---
+++ src/components/Dialog/index.ts
@@ -0,0 +1,4 @@
+export { useDialog } from './hooks/useDialog';
+export { useDialogPromise, DialogResponse } from './hooks/useDialogPromise';
+export { Dialog } from './Dialog';
+export type { Props } from './Dialog';
