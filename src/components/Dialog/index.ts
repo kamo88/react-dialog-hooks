bf4803cd7145ac8537edfaddff7b9f7903886543
kamo88 <62532951+kamo88@users.noreply.github.com>
2023-08-26 15:20:50 +0900

    Revert "Feature/create dialog component (#9)"
    This reverts commit fd151aad541fc0944c0aad5967909e89198e1f58.

--- src/components/Dialog/index.ts
+++
@@ -1,4 +0,0 @@
-export { useDialog } from './hooks/useDialog';
-export { useDialogPromise, DialogResponse } from './hooks/useDialogPromise';
-export { Dialog } from './Dialog';
-export type { Props } from './Dialog';
