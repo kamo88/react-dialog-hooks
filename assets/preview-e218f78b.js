b9ae083d6b9c9365f557130379aafea048b2a18b
kamo88 <kamo88@users.noreply.github.com>
2023-08-26 02:51:06 +0000

    deploy: ab2dc37004c5e21e14f7c4ec4f3862344930d8c2

---
+++ assets/preview-e218f78b.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-64a8cf87.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-d0bf3f51.js"),["./DocsRenderer-3PUGWF3O-d0bf3f51.js","./iframe-64a8cf87.js","./index-61bf1805.js","./_commonjsHelpers-de833af9.js","./react-18-ba2f8a3a.js","./index-6a5bd4ef.js","./index-b219d5f7.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-e218f78b.js.map
